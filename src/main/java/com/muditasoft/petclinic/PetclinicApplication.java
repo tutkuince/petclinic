732c2f348565d50a0f437c48a92c336b3cb7ce2e
tutkuince <tutku.ince@outlook.com>
2019-03-13 22:41:35 +0300

    @ServletComponentScan appended

--- src/main/java/com/muditasoft/petclinic/PetclinicApplication.java
+++ src/main/java/com/muditasoft/petclinic/PetclinicApplication.java
@@ -3,7 +3,9 @@ package com.muditasoft.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @EnableConfigurationProperties(value = {PetclinicProperties.class})
 @SpringBootApplication
 public class PetclinicApplication {

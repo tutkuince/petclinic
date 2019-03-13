c6bf75509cce99f642b1ec6e40fc14c42399897b
tutkuince <tutku.ince@outlook.com>
2019-03-13 19:01:01 +0300

    PetRepository interface

---
+++ src/main/java/com/muditasoft/petclinic/dao/PetRepository.java
@@ -0,0 +1,16 @@
+package com.muditasoft.petclinic.dao;
+
+
+import com.muditasoft.petclinic.model.Pet;
+
+import java.util.List;
+
+public interface PetRepository {
+    List<Pet> findAll();
+    Pet findById(Long id);
+    List<Pet> findBySurname(String surname);
+    void create(Pet pet);
+    Pet update(Pet pet);
+    void delete(Long id);
+    void deleteByOwnerId(Long id);
+}

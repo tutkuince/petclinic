fa91a746763a881b0e7dedadfaffaeb197a937b4
tutkuince <tutku.ince@outlook.com>
2019-03-13 18:48:19 +0300

    class of model layer

---
+++ src/main/java/com/muditasoft/petclinic/model/Owner.java
@@ -0,0 +1,53 @@
+package com.muditasoft.petclinic.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Owner {
+    private Long id;
+    private String name;
+    private String surname;
+
+    private Set<Pet> pets = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/muditasoft/petclinic/model/Pet.java
@@ -0,0 +1,53 @@
+package com.muditasoft.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private Long id;
+    private String name;
+    private LocalDate birthDate;
+
+    private Owner owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", owner=" + owner +
+                '}';
+    }
+}

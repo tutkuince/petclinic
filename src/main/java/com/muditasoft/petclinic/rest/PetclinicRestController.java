990df2230f2ef38d3ae6c1a5cdbe7af30c181c1a
tutkuince <tutku.ince@outlook.com>
2019-03-14 15:14:59 +0300

    getOwner with id

--- src/main/java/com/muditasoft/petclinic/rest/PetclinicRestController.java
+++ src/main/java/com/muditasoft/petclinic/rest/PetclinicRestController.java
@@ -1,13 +1,11 @@
 package com.muditasoft.petclinic.rest;
 
+import com.muditasoft.petclinic.exception.OwnerNotFoundException;
 import com.muditasoft.petclinic.model.Owner;
 import com.muditasoft.petclinic.service.PetclinicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,17 @@ public class PetclinicRestController {
     public ResponseEntity<List<Owner>> getOwners(@RequestParam("sn") String surname) {
         List<Owner> owners = petclinicService.findOwners(surname);
         return ResponseEntity.ok(owners);
+    }
+
+    @GetMapping("/owner/{id}")
+    public ResponseEntity<Owner> getOwner(@PathVariable("id") Long id) {
+        try {
+            Owner owner = petclinicService.findOwner(id);
+            return ResponseEntity.ok(owner);
+        } catch (OwnerNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+
     }
 }

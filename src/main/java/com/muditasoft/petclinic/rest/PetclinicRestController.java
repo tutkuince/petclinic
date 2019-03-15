4c76b5e3137ab3713ad73beba6c152a51ae76fbe
tutkuince <tutku.ince@outlook.com>
2019-03-15 14:05:50 +0300

    createOwner

--- src/main/java/com/muditasoft/petclinic/rest/PetclinicRestController.java
+++ src/main/java/com/muditasoft/petclinic/rest/PetclinicRestController.java
@@ -4,9 +4,12 @@ import com.muditasoft.petclinic.exception.OwnerNotFoundException;
 import com.muditasoft.petclinic.model.Owner;
 import com.muditasoft.petclinic.service.PetclinicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -37,7 +40,19 @@ public class PetclinicRestController {
         } catch (OwnerNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
+    }
 
+    // After creation process, return owner's uri
+    @PostMapping("/owner")
+    public ResponseEntity<URI> createOwner(@RequestBody Owner owner) {
+        try {
+            petclinicService.createOwner(owner);
+            Long id = owner.getId();
+            final URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+            return ResponseEntity.created(location).build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
 
     }
 }

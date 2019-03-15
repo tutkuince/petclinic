1a8804773f74aab91f93e371b878855b43301297
tutkuince <tutku.ince@outlook.com>
2019-03-15 14:27:19 +0300

    testDeleteOwner

--- src/test/java/com/muditasoft/petclinic/rest/PetclinicRestControllerTest.java
+++ src/test/java/com/muditasoft/petclinic/rest/PetclinicRestControllerTest.java
@@ -1,11 +1,13 @@
 package com.muditasoft.petclinic.rest;
 
 import com.muditasoft.petclinic.model.Owner;
+import org.assertj.core.api.Assertions;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -76,5 +78,17 @@ public class PetclinicRestControllerTest {
         owner = restTemplate.getForObject("http://localhost:8080/rest/owner/2", Owner.class);
 
         MatcherAssert.assertThat(owner.getSurname(), Matchers.equalTo("Ince"));
+    }
+
+    @Test
+    public void testDeleteOwner() {
+        restTemplate.delete("http://localhost:8080/rest/owner/4");
+
+        try {
+            restTemplate.getForEntity("http://localhost:8080/rest/owner/4", Owner.class);
+            Assertions.fail("should have not returned owner!");
+        } catch (RestClientException e) {
+
+        }
     }
 }

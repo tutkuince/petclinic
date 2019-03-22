bea542977913d513d97d0681c4f90fe09cb1cb2e
tutkuince <tutku.ince@outlook.com>
2019-03-22 21:20:17 +0300

    OwnerRepositoryJpaImpl

--- src/main/java/com/muditasoft/petclinic/dao/jpa/OwnerRepositoryJpaImpl.java
+++ src/main/java/com/muditasoft/petclinic/dao/jpa/OwnerRepositoryJpaImpl.java
@@ -4,38 +4,49 @@ import com.muditasoft.petclinic.dao.OwnerRepository;
 import com.muditasoft.petclinic.model.Owner;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class OwnerRepositoryJpaImpl implements OwnerRepository {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public OwnerRepositoryJpaImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public List<Owner> findAll() {
-        return null;
+        return entityManager.createQuery("from Owner", Owner.class).getResultList();
     }
 
     @Override
     public Owner findById(Long id) {
-        return null;
+        return entityManager.find(Owner.class, id);
     }
 
     @Override
     public List<Owner> findBySurname(String surname) {
-        return null;
+        return entityManager.createQuery("from Owner where surname =:surname", Owner.class)
+                .setParameter("surname", surname)
+                .getResultList();
     }
 
     @Override
     public void create(Owner owner) {
-
+        entityManager.persist(owner);
     }
 
     @Override
     public Owner update(Owner owner) {
-        return null;
+        return entityManager.merge(owner);
     }
 
     @Override
     public void delete(Long id) {
-
+        entityManager.remove(entityManager.getReference(Owner.class, id));
     }
 }

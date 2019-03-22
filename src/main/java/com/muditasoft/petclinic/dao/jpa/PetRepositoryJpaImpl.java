0aebd7f4f7a9b90638213584c304668902815559
tutkuince <tutku.ince@outlook.com>
2019-03-22 21:19:46 +0300

    PetRepositoryJpaImpl

---
+++ src/main/java/com/muditasoft/petclinic/dao/jpa/PetRepositoryJpaImpl.java
@@ -0,0 +1,57 @@
+package com.muditasoft.petclinic.dao.jpa;
+
+import com.muditasoft.petclinic.dao.PetRepository;
+import com.muditasoft.petclinic.model.Pet;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class PetRepositoryJpaImpl implements PetRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public PetRepositoryJpaImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Pet> findAll() {
+        return entityManager.createQuery("from Pet", Pet.class).getResultList();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return entityManager.find(Pet.class, id);
+    }
+
+    @Override
+    public List<Pet> findOwnerId(Long id) {
+        return entityManager.createQuery("from Pet where owner.id =: id", Pet.class).setParameter("id", id).getResultList();
+    }
+
+    @Override
+    public void create(Pet pet) {
+        entityManager.persist(pet);
+    }
+
+    @Override
+    public Pet update(Pet pet) {
+        return entityManager.merge(pet);
+    }
+
+    @Override
+    public void delete(Long id) {
+        entityManager.remove(entityManager.getReference(Pet.class, id));
+    }
+
+    @Override
+    public void deleteByOwnerId(Long id) {
+        entityManager.createQuery("delete from Pet where owner.id =: id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+}

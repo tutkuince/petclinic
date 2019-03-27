f7f9019750b55c0614ed62fedcf61cffa9d3969a
tutkuince <tutku.ince@outlook.com>
2019-03-27 16:55:51 +0300

    createOwners

--- src/main/java/com/muditasoft/petclinic/controller/PetclinicController.java
+++ src/main/java/com/muditasoft/petclinic/controller/PetclinicController.java
@@ -1,5 +1,6 @@
 package com.muditasoft.petclinic.controller;
 
+import com.muditasoft.petclinic.model.Owner;
 import com.muditasoft.petclinic.service.PetclinicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,32 @@ public class PetclinicController {
     @GetMapping("/owners")
     public String getOwners(Model model) {
         model.addAttribute("owners", petclinicService.findOwners());
+        return "index";
+    }
+
+    @GetMapping("/createOwners")
+    public String createOwners() {
+        Owner owner1 = new Owner();
+        owner1.setName("Tutku");
+        owner1.setSurname("Ince");
+
+        Owner owner2 = new Owner();
+        owner2.setName("Emin");
+        owner2.setSurname("Koklu");
+
+        Owner owner3 = new Owner();
+        owner3.setName("Ugur");
+        owner3.setSurname("Batikan");
+
+        Owner owner4 = new Owner();
+        owner4.setName("Alper");
+        owner4.setSurname("Omeroglu");
+
+        petclinicService.createOwner(owner1);
+        petclinicService.createOwner(owner2);
+        petclinicService.createOwner(owner3);
+        petclinicService.createOwner(owner4);
+
         return "index";
     }
 }

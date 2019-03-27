637af637a58b9fceab1c218825a3696f2a340cf4
tutkuince <tutku.ince@outlook.com>
2019-03-27 21:43:13 +0300

    UserDetailsService

--- src/main/java/com/muditasoft/petclinic/SecurityConfiguration.java
+++ src/main/java/com/muditasoft/petclinic/SecurityConfiguration.java
@@ -1,16 +1,26 @@
 package com.muditasoft.petclinic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfiguration(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**/favicon.ico", "/css/**", "/js/**", "/images/**", "/webjars/**", "/login").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin().loginPage("/login").loginProcessingUrl("/login").failureUrl("/login?loginFailed=true");
+        http.rememberMe().userDetailsService(userDetailsService);
     }
 }

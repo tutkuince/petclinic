66ff11abe3998183edd9414f8083e6c3fa9322f4
tutkuince <tutku.ince@outlook.com>
2019-03-27 22:59:10 +0300

    accessing pages with Roles

--- src/main/java/com/muditasoft/petclinic/SecurityConfiguration.java
+++ src/main/java/com/muditasoft/petclinic/SecurityConfiguration.java
@@ -24,6 +24,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**/favicon.ico", "/css/**", "/js/**", "/images/**", "/webjars/**", "/login").permitAll();
+        http.authorizeRequests().antMatchers("/rest/**").access("hasRole('EDITOR')");
+        http.authorizeRequests().antMatchers("/actuator/**").access("hasRole('ADMIN')");
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin().loginPage("/login").loginProcessingUrl("/login").failureUrl("/login?loginFailed=true");
         http.rememberMe().userDetailsService(userDetailsService);

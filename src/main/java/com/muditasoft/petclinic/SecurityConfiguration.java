892ab42decec142594b6da8ecaface083f818ad4
tutkuince <tutku.ince@outlook.com>
2019-03-29 18:02:14 +0300

    SecurityConfiguration extends AbstractSecurityConfiguration

--- src/main/java/com/muditasoft/petclinic/SecurityConfiguration.java
+++ src/main/java/com/muditasoft/petclinic/SecurityConfiguration.java
@@ -2,37 +2,21 @@ package com.muditasoft.petclinic;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-import javax.sql.DataSource;
-
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private UserDetailsService userDetailsService;
-    private DataSource dataSource;
+public class SecurityConfiguration extends AbstractSecurityConfiguration {
 
     @Autowired
-    public SecurityConfiguration(UserDetailsService userDetailsService, DataSource dataSource) {
-        this.userDetailsService = userDetailsService;
-        this.dataSource = dataSource;
-    }
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**/favicon.ico", "/css/**", "/js/**", "/images/**", "/webjars/**", "/login").permitAll();
-        http.authorizeRequests().antMatchers("/rest/**").access("hasRole('EDITOR')");
         http.authorizeRequests().antMatchers("/actuator/**").access("hasRole('ADMIN')");
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin().loginPage("/login").loginProcessingUrl("/login").failureUrl("/login?loginFailed=true");
         http.rememberMe().userDetailsService(userDetailsService);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource);
     }
 }

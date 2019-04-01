3d109b4d53c3abec9f915a9dadc72552361418b2
tutkuince <tutku.ince@outlook.com>
2019-04-01 21:03:27 +0300

    PetClinicSecurityWithInvalidAuthTokenTests

---
+++ src/test/java/com/muditasoft/petclinic/PetClinicSecurityWithInvalidAuthTokenTests.java
@@ -0,0 +1,37 @@
+package com.muditasoft.petclinic;
+
+import com.muditasoft.petclinic.service.PetclinicService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = "spring.profiles.active=dev")
+public class PetClinicSecurityWithInvalidAuthTokenTests {
+
+    @Autowired
+    private PetclinicService petclinicService;
+
+    @Before
+    public void setUp() {
+        TestingAuthenticationToken auth = new TestingAuthenticationToken("user", "secret", "ROLE_XXX");
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    @After
+    public void tearDown() {
+        SecurityContextHolder.clearContext();
+    }
+
+    @Test(expected = AccessDeniedException.class)
+    public void testFindOwners() {
+        petclinicService.findOwners();
+    }
+}
